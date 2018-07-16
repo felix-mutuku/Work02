9e8427ca4e7e66987f4abc726a1f1089d70c03ed
Felix Mutuku <38686045+felix-mutuku@users.noreply.github.com>
2018-07-16 17:14:47 +0300

    Updated Report Module

--- app/src/main/java/ke/co/ekenya/www/brooksidedairy/Activities/ReportModuleActivity.java
+++ app/src/main/java/ke/co/ekenya/www/brooksidedairy/Activities/ReportModuleActivity.java
@@ -1,16 +1,43 @@
 package ke.co.ekenya.www.brooksidedairy.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.ImageView;
 
 import ke.co.ekenya.www.brooksidedairy.R;
 
 public class ReportModuleActivity extends AppCompatActivity {
+    CardView specify_criteria;
+    ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_module);
 
+        specify_criteria = findViewById(R.id.specify_criteria);
+        back = findViewById(R.id.back);
+
+        specify_criteria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSpecifyCriteria();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void goToSpecifyCriteria() {
+        Intent intent = new Intent(ReportModuleActivity.this, ReportModuleCriteriaActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/ke/co/ekenya/www/brooksidedairy/Activities/ReportModuleCriteriaActivity.java
@@ -0,0 +1,17 @@
+package ke.co.ekenya.www.brooksidedairy.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ke.co.ekenya.www.brooksidedairy.R;
+
+public class ReportModuleCriteriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_module_criteria);
+
+
+    }
+}
